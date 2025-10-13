0be6836dd30a0db03941e190563475ccc0ca7d00
DarkTempest <jorgeandresmartinezsantos@gmail.com>
2025-10-12 22:11:21 -0500

    Doceceavo commit: Se agrega el controlador de User.

---
+++ src/main/java/edu/politienda/Controllers/userController.java
@@ -0,0 +1,75 @@
+package edu.politienda.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import edu.politienda.Models.ENTITY.user;
+import edu.politienda.Services.userService;
+
+@Controller
+@RequestMapping("/Usuarios")
+public class userController {
+    private final userService servicioControlar;
+
+    public userController(userService servicioControlar){
+        this.servicioControlar=servicioControlar;
+    }
+
+    @GetMapping
+    public String listar(Model model){
+        model.addAttribute("Usuarios", servicioControlar.listUsers());
+        return "Usuarios"; 
+    }
+
+    @GetMapping("/UsuariosNuevos")
+    public String nuevo(Model model){
+        model.addAttribute("Usuario", new user());
+        return "formUsuarios";
+    }
+
+     @GetMapping("/EditarUsuario/{id}")
+    public String editar(@PathVariable Long id, Model model, RedirectAttributes flash){
+        var u = servicioControlar.buscarPorId(id);
+        if (u == null){
+            flash.addFlashAttribute("error", "El Usuario que intentas editar no existe.");
+            return "redirect:/Usuarios";
+        }
+        model.addAttribute("Usuario", u);
+        model.addAttribute("titulo", "Editando Usuario: " + u.getNombre());
+        return "formUsuarios";
+    }
+
+     @PostMapping("/GuardarUsuarios")
+    public String guardar(@ModelAttribute user u, RedirectAttributes flash){
+        try {
+            servicioControlar.guardarUsuario(u);
+            flash.addFlashAttribute("exito", "El usuario ha sido guardado de manera perfecta.");
+            return "redirect:/Usuarios";
+        } catch (IllegalArgumentException e) {
+            flash.addFlashAttribute("error", e.getMessage());
+            flash.addFlashAttribute("Usuario", u);
+            return "redirect:/Usuarios/UsuariosNuevos";
+        }
+    }
+
+    @GetMapping("/eliminarUsuario/{id}")
+    public String eliminar(@PathVariable Long id, RedirectAttributes flash){
+        servicioControlar.eliminarUsuarios(id);
+        flash.addFlashAttribute("warning", "El Usuario fue eliminado.");
+        return "redirect:/Usuarios";
+    }
+
+    @GetMapping("/buscarUsuarios")
+    public String buscarPorNombre(@RequestParam("nombre") String nombre, Model model) {
+        model.addAttribute("Usuarios", servicioControlar.buscarPorNombre(nombre));
+        model.addAttribute("titulo", "Resultados para: " + nombre);
+        return "Usuarios";
+    }
+
+}
